6dbe914b1ecb631066cfc6a2c16fd7bc5428466b
Kevinlzs <kevinlzs.04@gmail.com>
2023-05-01 16:44:31 -0700

    Added image filter code

--- qt.py
+++ qt.py
@@ -8,6 +8,54 @@ from PySide6.QtCore import Slot
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QDesktopServices
 
+class NewWindow(QWidget):
+    def __init__(self, filename, type):
+        super().__init__()
+        self.label = QLabel()
+        img = Image.open(filename)
+        if type == "grayscale":
+            grayscale_list = [ ((a[0]+a[1]+a[2])//3, ) * 3 for a in img.getdata() ]
+            img.putdata(grayscale_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500,500, Qt.KeepAspectRatio)
+        elif type == "negative":
+            negative_list = [ (255-p[0], 255-p[1], 255-p[2]) for p in img.getdata() ]
+            img.putdata(negative_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500, 500, Qt.KeepAspectRatio)
+        elif type == "sepia":
+            sepia_list = [ (int(p[0]*1.1), p[1], int(p[2]*.9)) if p[0] < 63 else (int(p[0]*1.15), p[1], int(p[2]*.85)) if p[0] > 62 and p[0] < 192 else (int(p[0]*1.08), p[1], int(p[2]*.5)) for p in img.getdata()]
+            img.putdata(sepia_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500, 500, Qt.KeepAspectRatio)
+        elif type == "warm":
+            warm_list = [ (int(p[0] * 1.2), p[1], int(p[2] * .8)) for p in img.getdata() ]
+            img.putdata(warm_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500, 500, Qt.KeepAspectRatio)
+        elif type == "cool":
+            cool_list = [ (int(p[0] * .8), p[1], int(p[2] * 1.2)) for p in img.getdata() ]
+            img.putdata(cool_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500, 500, Qt.KeepAspectRatio)
+        elif type == "lark":
+            # lark_list = [ (int(p[0] * 1.2), int(p[1]*.9), int(p[2] * .8)) for p in img.getdata() ]
+            # infarred_list = [ (int(p[0] * .5), int(p[1]*.2), int(p[2] * .5)) for p in img.getdata() ]
+            thermal_list = [ (int(p[0] * .5), int(p[1]*.5), int(p[2] * 1.5)) for p in img.getdata() ]
+            img.putdata(thermal_list)
+            qim = ImageQt(img)
+            pixmap = QPixmap.fromImage(qim)
+            pixmap = pixmap.scaled(500, 500, Qt.KeepAspectRatio)
+        self.label.setPixmap(pixmap)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
+
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
